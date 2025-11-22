b089ed04766bb0879f901e075fa453baea26605c
Danilo <danilocbjp@hotmail.com>
2025-11-22 10:01:53 -0300

    update random alpha generator

--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -16,17 +16,13 @@ int main(int argc, char **argv) {
     MLP mlp = MLP(data);
 
     size_t max_iterations = 10;
-    size_t max_ils_iterations = static_cast<size_t>(min(data.getDimension(), 100));
-    
-    std::vector<double> alphas;
-    for (int i = 0; i <= 25; i++)
-        alphas.push_back( i / 100.0 );
+    size_t max_ils_iterations = static_cast<size_t>(min(data.getDimension(), 100));;
     
     std::random_device rd;
     std::mt19937 rng(rd());
     std::uniform_int_distribution<> dist(0, 25);
     
-    double alpha = alphas[dist(rng)];
+    double alpha = dist(rng) / 100.0;
 
     Solution s = mlp.GILS_RNVD(max_iterations, max_ils_iterations, alpha);
     s.print();
