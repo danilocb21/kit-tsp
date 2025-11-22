a8f36d9969151b0bcd14f11d6bb20dd351178c3c
Danilo <danilocbjp@hotmail.com>
2025-11-22 19:54:14 -0300

    update benchmark

--- MLP/benchmark.sh
+++ MLP/benchmark.sh
@@ -1,14 +1,14 @@
-make
+meson compile -C build
 
 k=1
 
-for instance in instances/*; do
+for instance in data/instances/*; do
 	echo $instance >> ./output.txt
 	
 	echo "Processando $instance"
 	echo "Instance $k of 22"
 	
-	./tsp ${instance} >> ./output.txt
+	./build/src/mlp ${instance} >> ./output.txt
 	
 	k=$(($k+1))
 done
--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <chrono>
 
 #include "data.h"
 #include "MLP.hpp"
@@ -21,11 +22,25 @@ int main(int argc, char **argv) {
     std::random_device rd;
     std::mt19937 rng(rd());
     std::uniform_int_distribution<> dist(0, 25);
-    
-    double alpha = dist(rng) / 100.0;
 
-    Solution s = mlp.GILS_RNVD(max_iterations, max_ils_iterations, alpha);
-    s.print();
+    size_t runs = 10;
+    double total_time = 0.0, total_cost = 0.0;
+
+    for (size_t i = 0; i < runs; i++) {
+        double alpha = dist(rng) / 100.0;
+
+        auto start = chrono::high_resolution_clock::now();
+
+        Solution s = mlp.GILS_RNVD(max_iterations, max_ils_iterations, alpha);
+
+        auto end = chrono::high_resolution_clock::now();
+
+        chrono::duration<double> duration = end - start;
+        total_time += duration.count();
+        total_cost += s.cost;
+    }
+    
+    std::cout << std::fixed << total_time / runs << ' ' << total_cost / runs << "\n\n";
 
     return 0;
 }
